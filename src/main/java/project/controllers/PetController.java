72a96cd549b5cdab0135d3c36526a4def0ef0a1f
Drago <88schweppes888@gmail.com>
2022-07-09 12:28:15 +0300

    the "/pet/add" directory broke the form.css for some reason. "/pet" link works fine

--- src/main/java/project/controllers/PetController.java
+++ src/main/java/project/controllers/PetController.java
@@ -1,26 +1,40 @@
 package project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import project.entities.dtos.AddPetDTO;
+import project.services.PetService;
 
 import javax.validation.Valid;
 
 @Controller
 public class PetController {
 
+    private PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @ModelAttribute("addPetDTO")
     public AddPetDTO initPetRegister(){
+
         return new AddPetDTO();
     }
 
 
     @GetMapping("/pet")
-    public String addPet(){
+    public String addPet(Model model){
+
+        if (!model.containsAttribute("addOfferModel")){
+            model.addAttribute("addOfferModel", new AddPetDTO());
+        }
+        model.addAttribute("types", petService.getTypes());
         return "pet-add";
     }
 
--- src/main/java/project/services/PetService.java
+++ src/main/java/project/services/PetService.java
@@ -1,9 +1,26 @@
 package project.services;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import project.entities.dtos.AddPetDTO;
+import project.entities.TypeEntity;
+import project.repositories.TypeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PetService {
+    private TypeRepository typeRepository;
+
+    public PetService(TypeRepository typeRepository) {
+        this.typeRepository = typeRepository;
+    }
+
+    public List<TypeEntity> getTypes() {
+
+
+        List<TypeEntity> types = new ArrayList<>(typeRepository.findAll());
+
+        System.out.println();
+        return types;
+    }
 }
